9d771741f5a0de618af96e170b6b78738424328b
MJ <javier.desarrollador@hotmail.com>
2020-11-22 13:29:23 -0600

    Add project files.

---
+++ QueueSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> carsMaintenance = new Queue<string>();
+
+            //adding values to the queue
+            carsMaintenance.Enqueue("Nissan");
+            carsMaintenance.Enqueue("Toyota");
+            carsMaintenance.Enqueue("Chevrolet");
+            carsMaintenance.Enqueue("Suzuki");
+            carsMaintenance.Enqueue("Hyundai");
+
+            int carsInQueue = carsMaintenance.Count;
+
+            for(int i=0; i<carsInQueue; i++)
+            { 
+                Console.WriteLine("Number of cars in queue: {0}", carsMaintenance.Count);
+                Console.WriteLine("Car on maintenance: {0}", carsMaintenance.Peek());
+                carsMaintenance.Dequeue(); //removing a value from the queue
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
